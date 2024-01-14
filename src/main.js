c7f64a3f5db76ef714a8d5f3a20642c200217a66
李建维 <lijianwei@hualala.com>
2024-01-14 14:07:37 +0800

    add gitigrou

--- src/main.js
+++ src/main.js
@@ -4,9 +4,6 @@ import Vue from "vue";
 import App from "./App";
 import router from "./router";
 import axios from "axios";
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
 axios.defaults.baseURL = "/api"
 Vue.prototype.$http = axios;
 /* eslint-disable no-new */
